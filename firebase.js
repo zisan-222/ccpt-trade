f729c053dd2ed563c343ebe1136147cef7a28d70
zisan-222 <mdzisanzisan000@gmail.com>
2026-08-05 13:20:28 +0600

    Update Firebase imports and initialize Auth and Firestore

--- firebase.js
+++ firebase.js
@@ -1,21 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyApkKni5c826UoJVoD6pGHrzLYYvTEHK0M",
   authDomain: "ccpt-trade.firebaseapp.com",
   projectId: "ccpt-trade",
   storageBucket: "ccpt-trade.firebasestorage.app",
   messagingSenderId: "461751033270",
-  appId: "1:461751033270:web:7fdc3f9a206cbf2218b517",
-  measurementId: "G-V11RSJDECT"
+  appId: "1:461751033270:web:7fdc3f9a206cbf2218b517"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
