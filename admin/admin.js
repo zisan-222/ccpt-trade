63039144ba8ecc74e52bc0e021a6683e80ebadc4
zisan-222 <mdzisanzisan000@gmail.com>
2026-08-05 13:15:05 +0600

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -1,1 +1,31 @@
 
+document
+.getElementById("adminLoginForm")
+.addEventListener("submit", function (e) {
+
+e.preventDefault();
+
+const email =
+document.getElementById("adminEmail").value;
+
+const password =
+document.getElementById("adminPassword").value;
+
+const message =
+document.getElementById("loginMessage");
+
+message.innerHTML = "";
+
+if(email === "" || password === ""){
+
+message.innerHTML = "Please fill all fields.";
+
+return;
+
+}
+
+message.style.color="#00ff99";
+
+message.innerHTML="Checking Admin Login...";
+
+});
